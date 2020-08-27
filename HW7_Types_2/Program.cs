b9e5fd251dc88990b48c0f6659c48e9ac5b2be1d
Олександр Owl <ischenk1756@yandex.ua>
2020-08-28 02:17:26 +0300

    Добавьте файлы проекта.

---
+++ HW7_Types_2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW7_Types_2
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            State state1 = new State(); // Структура State
+            State state2 = new State();
+            state2.x = 1;
+            state2.y = 2;
+            state1 = state2;
+            state2.x = 5; // state1.x=1 по-прежнему
+            Console.WriteLine(state1.x); // 1
+            Console.WriteLine(state2.x); // 5
+
+            Country country1 = new Country(); // Класс Country
+            Country country2 = new Country();
+            country2.x = 1;
+            country2.y = 4;
+            country1 = country2;
+            country2.x = 7; // теперь и country1.x = 7, так как обе ссылки и country1 и country2 
+                            // указывают на один объект в хипе
+            Console.WriteLine(country1.x); // 7
+            Console.WriteLine(country2.x); // 7
+
+            Console.Read();
+        }
+        struct State
+        {
+            public int x;
+            public int y;
+            public Country country;
+        }
+        class Country
+        {
+            public int x;
+            public int y;
+        }
+    }
+}
---
+++ HW7_Types_2/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("HW7_Types_2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HW7_Types_2")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("b21e1b30-90b0-499a-85ec-6c1d4e02d9af")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
